cd289fa7b74c22ce80d3502286f0d245073257db
Anaïs Koussa <anaisnkoussa@gmail.com>
2022-06-28 23:07:41 +0200

    Add files

--- character-manager/dashboard.js
+++ character-manager/dashboard.js
@@ -1,42 +1,73 @@
 const url = "https://character-database.becode.xyz/characters";
 const methodGet = {
-    method : 'GET'
+    method: 'GET'
+};
+const dashboard = document.querySelector("#dashboard");
+const main = document.querySelector("main");
+const characterContainer = document.querySelector(".container");
+
+let getCharacterCard = (character) => {
+    return `
+            <h2>${character.name}</h2>
+            <img src = "data:image/png;base64,${character.image}">
+            <h3>${character.shortDescription}</h3>
+            <a id="a-${character.id}" class="a-dashboard" href="#">More informations</a>
+        `;
 };
 
-const characterContainer = document.querySelector(".container");
+let showDetailsCard = (character) => {
+    dashboard.remove();
+    let section = document.createElement("section");
+    section.classList.add("character-editor");
+    section.innerHTML = `
+        <h2>${character.name}</h2>
+        <img src = "data:image/png;base64,${character.image}">
+        <h3>${character.shortDescription}</h3>
+        <p>${character.description}</p>
+        <button id="button-modify" type="button">Modify</button>
+        <button id="button-delete" type="button">Delete</button>   
+    `;
+    main.appendChild(section);
+}
 
 fetch(url, methodGet)
     .then(response => response.json())
     .then(data => {
-        for (let i = 0; i < data.length; i ++) {
-        let section = document.createElement("section");
-        characterContainer.appendChild(section);
-        section.innerHTML =  `
-            <h2>${data[i].name}</h2>
-            <img src = "data:image/png;base64,${data[i].image}">
-            <h3>${data[i].shortDescription}</h3>
-            <a class="a-dashboard" href="#">More informations</a>
-        `
-        }
-        const aEditors = document.querySelectorAll(".a-dashboard");
-        const dashboard = document.querySelector("#dashboard");
-        const main = document.querySelector("main");
-        aEditors.forEach(a => {
-            a.addEventListener("click", () => {
-                console.log(aEditors)
-                console.log(a)
-                dashboard.remove();
-                let section = document.createElement("section");
-                main.appendChild(section);
-                section.classList.add("character-editor");
-                section.innerHTML = `
-                    <h2>${data[0].name}</h2>
-                    <img src = "data:image/png;base64,${data[0].image}">
-                    <h3>${data[0].shortDescription}</h3>
-                    <p>${data[0].description}</p>
-                    <button id="button-modify" type="button">Modify</button>
-                    <button id="button-delete" type="button">Delete</button>   
-                `
-            })   
-        });
-    })
+        for (let i = 0; i < data.length; i++) {
+            let character = data[i];
+            let section = document.createElement("section");
+            section.id = 'card-' + character.id;
+            section.innerHTML = getCharacterCard(character);
+            characterContainer.appendChild(section);
+            let a = document.querySelector(`#a-${character.id}`);
+            a.addEventListener('click', (e) => {
+            e.preventDefault(); 
+            showDetailsCard(character);
+            });
+        }    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

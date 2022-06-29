da47790a1ac1292f66d83a9ae0a6531170e85d7b
Anaïs Koussa <anaisnkoussa@gmail.com>
2022-06-29 16:57:36 +0200

    Add files

--- character-manager/dashboard.js
+++ character-manager/dashboard.js
@@ -38,6 +38,66 @@ let showDetailsCard = (character) => {
             `
         } 
     })
+
+    const modifyButton = document.querySelector("#button-modify");
+    modifyButton.addEventListener('click', () => {
+        const section = document.createElement("section");
+        main.appendChild(section);
+        section.classList.add("character-creaction");
+        section.innerHTML = `
+        <form>
+            <label for="name"></label>Name :<br>
+            <input type="text" id="name" name="name"><br><br>
+            <label for="sdescription">Short Description :</label><br>
+            <textarea id="sDescription" type="text"></textarea><br><br>
+            <label for="description">Description :</label><br>
+            <textarea id="description"></textarea><br><br>
+            <label for="image"></label>Image :<br>
+            <input type="file" id="image" name="image" accept=".jpg, .jpeg, .png"><br><br>
+            <button id="submit">Submit</button>
+        </form>
+        ` 
+    let button = document.querySelector("#submit");
+    let name = document.querySelector("#name");
+    let shortDescription = document.querySelector("#sDescription");
+    let description = document.querySelector("#description");
+    const file = document.querySelector("input[type=file]");
+    var base64String = "";
+    function Uploaded() {
+        var file = document.querySelector("input[type=file]")["files"][0];
+        var reader = new FileReader();
+        reader.onload = function () {
+            base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+        };
+        reader.readAsDataURL(file);
+    }
+
+    file.addEventListener("change", () => {
+        Uploaded();
+    });
+    button.addEventListener("click", () => {
+        console.log("yes")
+        fetch(`https://character-database.becode.xyz/characters/${character.id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name : name.value,
+                
+                shortDescription : shortDescription.value,
+                
+                description : description.value,
+
+                image : base64String
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        name.value = "";
+        shortDescription.value = "";
+        description.value = "";
+    })
+    })
+
 }
 
 fetch(url, methodGet)
